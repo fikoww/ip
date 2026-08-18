91826ea5cb82eeb8ae10c92581b8aa5fb8298766
Frederico Samuel Halim <fredericosamuel01@gmail.com>
2026-08-19 01:03:38 +0800

    Changed the name Duke to Puyo. And changes the initial codes

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Puyo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Puyo.java
@@ -0,0 +1,19 @@
+public class Puyo {
+    public static void main(String[] args) {
+        String banner = "██████╗ ██╗   ██╗██╗   ██╗ ██████╗\n" +
+                        "██╔══██╗██║   ██║╚██╗ ██╔╝██╔═══██╗\n" +
+                        "██████╔╝██║   ██║ ╚████╔╝ ██║   ██║\n" +
+                        "██╔═══╝ ██║   ██║  ╚██╔╝  ██║   ██║\n" +
+                        "██║     ╚██████╔╝   ██║   ╚██████╔╝\n" +
+                        "╚═╝      ╚═════╝    ╚═╝    ╚═════╝";
+        System.out.println("─".repeat(70));
+        System.out.println(banner);
+        System.out.println("─".repeat(70));
+        System.out.println("Hello! My name is Puyo!");
+        System.out.println("I'm a penguin!");
+        System.out.println("Regardless, I can do a lot of things to make your life easier!");
+        System.out.println("─".repeat(70));
+        System.out.println("Okay. See you again! Bye!");
+        System.out.println("─".repeat(70));
+    }
+}
