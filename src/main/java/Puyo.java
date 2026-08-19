ebc7c0a75bb9574e206259d863a5c1096bfdbe7a
Frederico Samuel Halim <fredericosamuel01@gmail.com>
2026-08-19 21:37:33 +0800

    A version where Puyo gives echo to the user's input, and when the user says 'bye', it exist the system

--- src/main/java/Puyo.java
+++ src/main/java/Puyo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Puyo {
     public static void main(String[] args) {
         String banner = "██████╗ ██╗   ██╗██╗   ██╗ ██████╗\n" +
@@ -6,14 +8,32 @@ public class Puyo {
                         "██╔═══╝ ██║   ██║  ╚██╔╝  ██║   ██║\n" +
                         "██║     ╚██████╔╝   ██║   ╚██████╔╝\n" +
                         "╚═╝      ╚═════╝    ╚═╝    ╚═════╝";
-        System.out.println("─".repeat(70));
+        System.out.println("━".repeat(70));
         System.out.println(banner);
-        System.out.println("─".repeat(70));
+        System.out.println("━".repeat(70));
         System.out.println("Hello! My name is Puyo!");
         System.out.println("I'm a penguin!");
         System.out.println("Regardless, I can do a lot of things to make your life easier!");
-        System.out.println("─".repeat(70));
-        System.out.println("Okay. See you again! Bye!");
-        System.out.println("─".repeat(70));
+        System.out.println("━".repeat(70));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("  Type something! (type 'bye' to exit)");
+        System.out.println("━".repeat(70));
+
+        while (true) {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+            System.out.println("─".repeat(70));
+
+            if (input.equals("bye")) {
+                System.out.println("Okay. See you again! Bye!");
+                break;
+            }
+
+            System.out.println(input);
+            System.out.println("─".repeat(70));
+        }
+
+        scanner.close();
     }
 }
