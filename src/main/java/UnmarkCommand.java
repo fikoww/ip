9960c7fce7c50245dfe375c006b23083bb579ecf
Frederico Samuel Halim <fredericosamuel01@gmail.com>
2026-08-27 20:57:27 +0800

    Add OOP#7: unmark command

--- src/main/java/UnmarkCommand.java
+++ src/main/java/UnmarkCommand.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+public class UnmarkCommand extends Command {
 
-public class UnmarkCommand {
+    private final int index;
+
+    public UnmarkCommand(int index) {
+        this.index = index;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws PuyoException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new PuyoException("Invalid task number.");
+        }
+        Task task = tasks.get(index);
+        if (!task.done) {
+            ui.showMessage(" You indeed haven't done this task!");
+        } else {
+            task.unmark();
+            ui.showMessage(" Don't forget to " + task.name + "!");
+            ui.showMessage(" " + task);
+            storage.save(tasks);
+        }
+    }
 }
